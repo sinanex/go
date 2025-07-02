929ac06f561826bcd492b80eee20b16ce030c576
Muhammad Sinan <ms4730435@gmail.com>
2025-07-02 14:50:16 +0530

    go

--- baciss.go
+++ baciss.go
@@ -5,9 +5,22 @@ import "fmt"
 func main() {
 
 	fmt.Println(add(18, 34))
+	//data types
 
+	var age int = 19
+
+	//uint -only positive nummbers
+
+	fmt.Println(age)
+
+	var name string = "sinan"
+	fmt.Println(message(name))
 }
 
 func add(a int, b int) int {
 	return a + b
+}
+
+func message(name string) string {
+	return fmt.Sprintf("hello %s", name)
 }
